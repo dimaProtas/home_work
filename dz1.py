5e0ffda5239372ed87edbac785133ee4966f958f
dima_protasevich92 <dima_protasevich92@mail.ru>
2021-12-17 12:11:24 +0300

    add dz1

---
+++ dz1.py
@@ -0,0 +1,31 @@
+duration = 154
+
+if duration < 60:
+    print(f'{duration} секунды')
+elif duration > 60 and duration < 3601:
+    mm = duration // 60
+    ss = duration % 60
+    print(f'{mm} минут, {ss} секунда')
+elif duration > 3600 and duration < 86401:
+    mm = duration // 60
+    hh = mm // 60
+    ss = duration % 60
+    if mm >= 60:
+        mm = mm % 10
+    print(f'{int(hh)} часов, {int(mm)} минут, {ss} секунд!')
+elif duration > 86400:
+    mm = duration // 60
+    hh = mm // 60
+    ss = duration % 60
+    dd = hh // 24
+    if mm >= 60 and hh >= 24:
+        hh = hh - dd * 24
+        mm = mm % 10
+    print(f'{dd} день, {int(hh)} часов, {int(mm)} минут, {ss} секунд!')
+
+
+
+
+
+
+
